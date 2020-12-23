ff491dbba31ed6834f82756e3639fe3b843420a6
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-23 10:13:47 +0800

    12/23

--- cookie/server.js
+++ cookie/server.js
@@ -4,17 +4,16 @@ const fs = require('fs')
 http.createServer(function(req,res){
   console.log('request come', res.url);
 
-  const host = req.headers.host
+  const host = req.headers.host //拿到host
 
   if(req.url === '/') {
     const html = fs.readFileSync('test.html', 'utf8') 
-    if (host === 'a.test.com'){
+    if (host === 'a.test.com'){ //满足条件写cookie
       res.writeHead(200, {
         'Content-Type': 'text/html',
         'Set-Cookie': ['id=123; max-age=2','abc=456; HttpOnly']
       }) 
     }
-    
     res.end(html)
   }
 
