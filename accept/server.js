d1d33ec94d1b6bdb82c2e6304368d6565ed1a250
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-24 11:08:40 +0800

    12/24

--- accept/server.js
+++ accept/server.js
@@ -8,7 +8,8 @@ http.createServer(function(req,res){
   const html = fs.readFileSync('test.html')
   res.writeHead(200, {
     'Content-Type': 'text/html',
-    'Content-Encoding': 'gzip' //压缩了body
+    // 'X-Content-Type-Options': 'nosniff' //不会主动预测返回的内容
+    'Content-Encoding': 'gzip' //压缩了body，实际传输大小会有变化(变小)
   })
   res.end(zlib.gzipSync(html))
 
