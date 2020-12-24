e2a3e9415e6d7da6bf446f82b8a1778176b3d028
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-24 15:28:17 +0800

    12/24

--- csp/server.js
+++ csp/server.js
@@ -4,12 +4,19 @@ const fs = require('fs')
 http.createServer(function(request, response) {
   console.log('request come', request.url);
 
-  const html = fs.readFileSync('test.html', 'utf8')
-  response.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Content-Security-Policy': ' default-src \'self\'; form-action \'self\''
-  })
-  response.end(html)
+  if(request.url === '/'){
+    const html = fs.readFileSync('test.html', 'utf8')
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Security-Policy': ' default-src http: https:; form-action \'self\''
+    })
+    response.end(html)
+  } else {
+    response.writeHead(200, {
+      'Content-Type': 'application/javascript'
+    })
+    response.end('console.log("loaded script")')
+  }
 }).listen(8888)
 
 console.log('server listening on 8888');
