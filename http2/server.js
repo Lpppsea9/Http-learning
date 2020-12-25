3a740fbe41a304432404af536e80079816c337c1
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-25 11:57:10 +0800

    12/25

---
+++ http2/server.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer((request,response) => {
+  console.log('request come', request.url);
+
+  const html = fs.readFileSync('test.html', 'utf8')
+  const img = fs.readFileSync('test.jpg')
+  if(request.url === '/') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Connection': 'keep-alive',
+      // Link是http2定义的 要推送可以定义哪些内容，下面推送test.jpg
+      // 要推送的test.jpg(绝对路径); 指定它的类型; 需要进行一个服务端的推送
+      'Link': '</test.jpg>; as=image; rel=preload'
+    })
+    response.end(html)
+  } else {
+    response.writeHead(200, {
+      'Content-Type': 'image/jpg',
+      'Connection': 'close'
+    })
+    response.end(img)
+  }
+})
